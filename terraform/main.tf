cdb2efddb879da0dd4c4a1986d8e0c35d5a96da0
AceS0 <abdulcelilsekerci@gmail.com>
2026-05-08 11:55:06 +0200

    fix(terraform): remove duplicate provider declarations in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,14 +16,6 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-    }
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
   }
 
   backend "azurerm" {
