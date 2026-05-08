160c6563b07cf19c5b992cd73c83004897e951d4
AceS0 <abdulcelilsekerci@gmail.com>
2026-05-08 13:27:18 +0200

    fix: Fixed terraform error (#165)

--- terraform/main.tf
+++ terraform/main.tf
@@ -191,20 +191,6 @@ resource "azurerm_network_security_rule" "whoknows_https_rule" {
   resource_group_name         = azurerm_resource_group.whoknows.name
 }
 
-resource "azurerm_network_security_rule" "whoknows_https_rule" {
-  name                        = "HTTPS"
-  priority                    = 300
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  network_security_group_name = azurerm_network_security_group.whoknows_nsg.name
-  resource_group_name         = azurerm_resource_group.whoknows.name
-}
-
 resource "azurerm_subnet_network_security_group_association" "whoknows_assoc" {
   subnet_id                 = azurerm_subnet.whoknows.id
   network_security_group_id = azurerm_network_security_group.whoknows_nsg.id
